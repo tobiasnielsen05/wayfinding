acd4a768161395b16aea0f0d8dca29f42beb7497
Tobias <ton001@edu.zealand.dk>
2024-04-09 11:07:12 +0200

    tilføjet map

--- index.php
+++ index.php
@@ -6,47 +6,66 @@ require "settings/init.php";
 <head>
 	<meta charset="utf-8">
 	
-	<title>Sigende titel</title>
-	
-	<meta name="robots" content="All">
+	<title>Flagra's Guide</title>
+
+    <meta name="robots" content="All">
 	<meta name="author" content="Udgiver">
 	<meta name="copyright" content="Information om copyright">
 	
 	<link href="css/styles.css" rel="stylesheet" type="text/css">
-	
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://api.mapbox.com/mapbox-gl-js/v3.2.0/mapbox-gl.css" rel="stylesheet">
+
+    <script type="module" src="./index.js"></script>
+    <script src="https://api.mapbox.com/mapbox-gl-js/v3.2.0/mapbox-gl.js"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
 <div class="row g-2">
-	<?php
-	$produkter = $db->sql("SELECT * FROM produkter");
-	foreach($produkter as $produkt) {
-		?>
-		<div class="col-12 col-md-6">
-			<div class="card w-100">
-				<div class="card-header">
-					<?php
-					echo $produkt->prodNavn;
-					?>
-				</div>
-				<div class="card-body">
-					<?php
-					// Indsæt andet felt fra database
-					?>
-				</div>
-				<div class="card-footer text-muted">
-					<?php
-					// Indsæt andet felt fra database
-					?>
-				</div>
-			</div>
-		</div>
-		<?php
-	}
-	?>
+    <div id="map">
+    </div>
 </div>
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script>
+    mapboxgl.accessToken = 'pk.eyJ1IjoidG9uMDAxIiwiYSI6ImNsdXMyZjJocjBnMHkya24wcXgwY2Z6Z3IifQ.IBOKpqMvOrn3_6oxvapjsA';
+    const map = new mapboxgl.Map({
+        container: 'map',
+        // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [11.8729232, 54.767849],
+        zoom: 12.5
+    });
+
+    // Create a default Marker and add it to the map.
+    const marker1 = new mapboxgl.Marker()
+        .setLngLat([11.8905921, 54.7668023])
+        .addTo(map);
+
+    const marker2 = new mapboxgl.Marker()
+        .setLngLat([11.862117, 54.770114])
+        .addTo(map);
+
+    const marker3 = new mapboxgl.Marker()
+        .setLngLat([11.848688, 54.7826153])
+        .addTo(map);
+
+    const marker4 = new mapboxgl.Marker()
+        .setLngLat([11.8615789, 54.7732872])
+        .addTo(map);
+
+    const marker5 = new mapboxgl.Marker()
+        .setLngLat([11.8708083, 54.7706267])
+        .addTo(map);
+
+    const marker6 = new mapboxgl.Marker()
+        .setLngLat([11.8771054, 54.76946])
+        .addTo(map);
+
+
+
+</script>
+
 </body>
 </html>
--- settings/init.php
+++ settings/init.php
@@ -9,7 +9,7 @@ define("CONFIG_LIVE", "0"); // 0: Test enviroment || 1: Live enviroment
 
 if(CONFIG_LIVE == 0){
     $DB_SERVER = "localhost";
-    $DB_NAME = "test";
+    $DB_NAME = "";
     $DB_USER = "root";
     $DB_PASS = "";
 }else{
