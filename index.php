ce3fa37ccfb92bb1640cd6975901fcea57d4454b
Tobias <ton001@edu.zealand.dk>
2024-04-16 11:41:25 +0200

    ændret primary farve

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
-<body>
+<body class="bg-primary">
 <div>
     <a href="maps.php"><img class="w-100" src="images/forside.jpg" alt="baggrundsbillede"></a>
 </div>
